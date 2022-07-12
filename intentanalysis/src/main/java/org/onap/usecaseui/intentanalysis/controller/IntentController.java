61921b6736371405813269eb657ec2ea4841e43b
ShuhaoCai <caishuhao@huawei.com>
2022-07-12 17:24:36 +0800

    Intent controller and service interface definition
    Issue-ID: USECASEUI-695
    Signed-off-by: ShuhaoCai <caishuhao@huawei.com>
    Change-Id: I94396df258441fa2a6cca9c792e3032d949d6ec5

---
+++ intentanalysis/src/main/java/org/onap/usecaseui/intentanalysis/controller/IntentController.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2022 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.usecaseui.intentanalysis.controller;
+
+import org.onap.usecaseui.intentanalysis.bean.models.Intent;
+import org.onap.usecaseui.intentanalysis.service.IntentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/intents")
+public class IntentController {
+
+    private static final String INTENT_ID = "intentId";
+
+    @Autowired
+    private IntentService intentService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Intent>> getIntentList() {
+        return ResponseEntity.ok(intentService.getIntentList());
+    }
+
+    @GetMapping(value = "/{intentId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Intent> getIntentById(
+            @PathVariable(INTENT_ID) String intentId) {
+        return ResponseEntity.ok(intentService.getIntentById(intentId));
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Intent> createIntent(@RequestBody Intent intent) {
+        return ResponseEntity.ok(intentService.createIntent(intent));
+    }
+
+    @PutMapping(value = "/{intentId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Intent> updateIntentById(
+            @PathVariable(INTENT_ID) String intentId,
+            @RequestBody Intent intent) {
+        return ResponseEntity.ok(intentService.updateIntentById(intentId, intent));
+    }
+
+    @DeleteMapping(value = "/{intentId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void removeIntentById(@PathVariable(INTENT_ID) String intentId) {
+        intentService.deleteIntentById(intentId);
+    }
+
+}
