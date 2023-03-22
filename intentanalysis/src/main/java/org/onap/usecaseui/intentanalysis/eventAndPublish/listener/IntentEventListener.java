4a90df529431588f3228c236aa17d2c11116328f
hekeguang <hekeguang@chinamobile.com>
2023-03-22 15:59:14 +0800

    Merge Event and Thread code.
    Issue-ID: USECASEUI-784
    Change-Id: I0cdef6c490f96c45e77013b0da5c372decdef6a1
    Signed-off-by: hekeguang <hekeguang@chinamobile.com>

---
+++ intentanalysis/src/main/java/org/onap/usecaseui/intentanalysis/eventAndPublish/listener/IntentEventListener.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2023 CMCC, Inc. and others. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.usecaseui.intentanalysis.eventAndPublish.listener;
+
+import org.apache.commons.lang.StringUtils;
+import org.onap.usecaseui.intentanalysis.bean.models.Context;
+import org.onap.usecaseui.intentanalysis.bean.models.Intent;
+import org.onap.usecaseui.intentanalysis.bean.models.IntentEventRecord;
+import org.onap.usecaseui.intentanalysis.bean.models.IntentGoalBean;
+import org.onap.usecaseui.intentanalysis.eventAndPublish.event.IntentCreateEvent;
+import org.onap.usecaseui.intentanalysis.intentBaseService.intentEventRecord.IntentEventRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class IntentEventListener {
+    @Autowired
+    private IntentEventRecordService intentEventRecordService;
+
+    @EventListener
+    public void listenIntentCreateEvent(IntentCreateEvent intentCreateEvent){
+        String intentStatus = intentCreateEvent.getIntentStatus();//create status
+        IntentGoalBean intentGoalBean = intentCreateEvent.getIntentGoalBean();//current operate intent
+        Intent currentIntent = intentGoalBean.getIntent();
+        List<Context> owner_info = currentIntent.getIntentContexts().stream().filter(x ->
+                StringUtils.equals(x.getContextName(), "owner info")).collect(Collectors.toList());
+        List<Context> handler_info = currentIntent.getIntentContexts().stream().filter(x ->
+                StringUtils.equals(x.getContextName(), "handler info")).collect(Collectors.toList());
+        String owner = owner_info.get(0).getContextConditions().get(0).getConditionValue();
+        String handler = handler_info.get(0).getContextConditions().get(0).getConditionValue();
+
+        IntentEventRecord intentEventRecord = new IntentEventRecord();
+        intentEventRecord.setIntentId(currentIntent.getIntentId());
+        intentEventRecord.setIntentName(currentIntent.getIntentName());
+        intentEventRecord.setIntentStatus(intentStatus);
+        intentEventRecord.setOperateType(intentGoalBean.getIntentGoalType().name());
+        intentEventRecordService.createIntentEventRecord(intentEventRecord);
+    }
+}
